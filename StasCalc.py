eb9892d51aa5e397593529aed222a52132c51d1b
StasSuhhonenko <47975675+StasSuhhonenko@users.noreply.github.com>
2019-02-25 14:33:24 +0300

    Add files via upload

---
+++ StasCalc.py
@@ -0,0 +1,24 @@
+import re
+print("Stanislav Suhhonenko Calc")
+print("Write 'quit' to exit the calculator\n")
+previous=0
+run=True
+def performMath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Введите пример:")
+    else:
+        equation = input(str(previous))
+    if equation == 'quit':
+        print("Thanks for using this software")
+        run=False
+    else:
+        equation = re.sub('[a-zA-Z,.:()""=%&@#$]', '', equation)
+        if previous == 0:
+             previous = eval(equation)
+        else:
+            previous = eval(str(previous)+equation)
+while run:
+    performMath()
